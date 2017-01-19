62a774c3e6cdbd950aaa4f5d8fe1bd8b248c5750
Luis Sanz <luis.sanz@gmail.com>
2017-01-19 19:20:22 +0100

    add Geom class
    The Geom class defines the geometry of shapes as far as the physics
    space is concerned. Unlike the previous model, it is not restricted to
    a 2x2x2 grid per block.

--- src/physics.cc
+++ src/physics.cc
@@ -9,6 +9,78 @@
 
 #include "world.h"
 
+class BlockGeom : public Geom {
+ public:
+	BlockGeom() : Geom() { }
+	~BlockGeom() { }
+
+	bool test_xpos(const v3ll &p, const box3f &bb, float *best)
+	{
+		box3f bb2(p.x, p.y, p.z, p.x + 1, p.y + 1, p.z + 1);
+		if (!overlap(bb, bb2))
+			return false;
+		if (*best < bb2.x0)
+			return false;
+		*best = bb2.x0;
+		return true;
+	}
+
+	bool test_xneg(const v3ll &p, const box3f &bb, float *best)
+	{
+		box3f bb2(p.x, p.y, p.z, p.x + 1, p.y + 1, p.z + 1);
+		if (!overlap(bb, bb2))
+			return false;
+		if (*best > bb2.x1)
+			return false;
+		*best = bb2.x1;
+		return true;
+	}
+
+	bool test_ypos(const v3ll &p, const box3f &bb, float *best)
+	{
+		box3f bb2(p.x, p.y, p.z, p.x + 1, p.y + 1, p.z + 1);
+		if (!overlap(bb, bb2))
+			return false;
+		if (*best < bb2.y0)
+			return false;
+		*best = bb2.y0;
+		return true;
+	}
+
+	bool test_yneg(const v3ll &p, const box3f &bb, float *best)
+	{
+		box3f bb2(p.x, p.y, p.z, p.x + 1, p.y + 1, p.z + 1);
+		if (!overlap(bb, bb2))
+			return false;
+		if (*best > bb2.y1)
+			return false;
+		*best = bb2.y1;
+		return true;
+	}
+
+	bool test_zpos(const v3ll &p, const box3f &bb, float *best)
+	{
+		box3f bb2(p.x, p.y, p.z, p.x + 1, p.y + 1, p.z + 1);
+		if (!overlap(bb, bb2))
+			return false;
+		if (*best < bb2.z0)
+			return false;
+		*best = bb2.z0;
+		return true;
+	}
+
+	bool test_zneg(const v3ll &p, const box3f &bb, float *best)
+	{
+		box3f bb2(p.x, p.y, p.z, p.x + 1, p.y + 1, p.z + 1);
+		if (!overlap(bb, bb2))
+			return false;
+		if (*best > bb2.z1)
+			return false;
+		*best = bb2.z1;
+		return true;
+	}
+};
+
 Body::Body(Space *s)
 {
 }
@@ -20,6 +92,13 @@ Body::~Body()
 Space::Space(World *w)
 	: world(w)
 {
+	memset(geom, 0, sizeof(geom));
+	geom[SHAPE_BLOCK_DN] = new BlockGeom();
+	geom[SHAPE_BLOCK_UP] = new BlockGeom();
+	geom[SHAPE_BLOCK_LF] = new BlockGeom();
+	geom[SHAPE_BLOCK_RT] = new BlockGeom();
+	geom[SHAPE_BLOCK_BK] = new BlockGeom();
+	geom[SHAPE_BLOCK_FT] = new BlockGeom();
 }
 
 Space::~Space()
@@ -59,6 +138,8 @@ static const int shape_masks[] = {
 	0xff,
 };
 
+
+
 int Space::cell_at(const int *masks, int64_t x, int64_t y, int64_t z)
 {
 	int shape, mask;
@@ -69,178 +150,240 @@ int Space::cell_at(const int *masks, int64_t x, int64_t y, int64_t z)
 
 void Space::move_xpos(Body *b, float dt)
 {
-	int64_t x, y, z, y0, y1, z0, z1;
-	int step_up = 0;
+	box3f bb;
+	box3ll bbc;
+	bool collide;
+	float best;
+	v3ll best_p;
 
-	x = floor((b->bb.x1 + b->v.x * dt) * 2);
-	y0 = floor(b->bb.y0 * 2);
-	z0 = floor(b->bb.z0 * 2);
-	y1 = floor(b->bb.y1 * 2);
-	z1 = floor(b->bb.z1 * 2);
-	for (y = y0; y <= y1; ++y) {
-		for (z = z0; z <= z1; ++z) {
-			if (cell_at(shape_masks, x, y, z)) {
-				if (y < y0 + b->step_size) {
-					step_up = 1;
-					continue;
-				}
-				b->v.x = 0;
-				b->p.x = 0.5 * x - b->s.x - impulse;
-				if (b->cb_func) {
-					v3ll p(x >> 1, y >> 1, z >> 1);
-					b->cb_func(b, p, FACE_LF);
-				}
-				return;
-			}
+	bb = b->bb;
+	bb.x0 = bb.x1;
+	bb.x1 += b->v.x * dt;
+
+	bbc.x0 = floor(bb.x0);
+	bbc.y0 = floor(bb.y0);
+	bbc.z0 = floor(bb.z0);
+	bbc.x1 = ceil(bb.x1);
+	bbc.y1 = ceil(bb.y1);
+	bbc.z1 = ceil(bb.z1);
+
+	collide = false;
+	best = bb.x1;
+
+	for (auto p : bbc) {
+		Geom *g = geom[world->get_shape(p)];
+		if (g != nullptr && g->test_xpos(p, bb, &best)) {
+			collide = true;
+			best_p = p;
 		}
 	}
-	if (step_up)
-		b->p.y = 0.5 * (y0 + b->step_size) + b->s.y + impulse;
+	if (collide) {
+		b->v.x = 0;
+		b->p.x = best - b->s.x - impulse;
+		if (b->cb_func)
+			b->cb_func(b, best_p, FACE_LF);
+		return;
+	}
 	b->p.x += b->v.x * dt;
 }
 
 void Space::move_xneg(Body *b, float dt)
 {
-	int64_t x, y, z, y0, y1, z0, z1;
-	int step_up = 0;
+	box3f bb;
+	box3ll bbc;
+	bool collide;
+	float best;
+	v3ll best_p;
 
-	x = floor((b->bb.x0 + b->v.x * dt) * 2);
-	y0 = floor(b->bb.y0 * 2);
-	z0 = floor(b->bb.z0 * 2);
-	y1 = floor(b->bb.y1 * 2);
-	z1 = floor(b->bb.z1 * 2);
-	for (y = y0; y <= y1; ++y) {
-		for (z = z0; z <= z1; ++z) {
-			if (cell_at(shape_masks, x, y, z)) {
-				if (y < y0 + b->step_size) {
-					step_up = 1;
-					continue;
-				}
-				b->v.x = 0;
-				b->p.x = 0.5 * (x + 1) + b->s.x + impulse;
-				if (b->cb_func) {
-					v3ll p(x >> 1, y >> 1, z >> 1);
-					b->cb_func(b, p, FACE_RT);
-				}
-				return;
-			}
+	bb = b->bb;
+	bb.x1 = bb.x0;
+	bb.x0 += b->v.x * dt;
+
+	bbc.x0 = floor(bb.x0);
+	bbc.y0 = floor(bb.y0);
+	bbc.z0 = floor(bb.z0);
+	bbc.x1 = ceil(bb.x1);
+	bbc.y1 = ceil(bb.y1);
+	bbc.z1 = ceil(bb.z1);
+
+	collide = false;
+	best = bb.x0;
+
+	for (auto p : bbc) {
+		Geom *g = geom[world->get_shape(p)];
+		if (g != nullptr && g->test_xneg(p, bb, &best)) {
+			collide = true;
+			best_p = p;
 		}
 	}
-	if (step_up)
-		b->p.y = 0.5 * (y0 + b->step_size) + b->s.y + impulse;
+	if (collide) {
+		b->v.x = 0;
+		b->p.x = best + b->s.x + impulse;
+		if (b->cb_func)
+			b->cb_func(b, best_p, FACE_RT);
+		return;
+	}
 	b->p.x += b->v.x * dt;
 }
 
 void Space::move_zpos(Body *b, float dt)
 {
-	int64_t x, y, z, x0, x1, y0, y1;
-	int step_up = 0;
+	box3f bb;
+	box3ll bbc;
+	bool collide;
+	float best;
+	v3ll best_p;
 
-	z = floor((b->bb.z1 + b->v.z * dt) * 2);
-	x0 = floor(b->bb.x0 * 2);
-	y0 = floor(b->bb.y0 * 2);
-	x1 = floor(b->bb.x1 * 2);
-	y1 = floor(b->bb.y1 * 2);
-	for (x = x0; x <= x1; ++x) {
-		for (y = y0; y <= y1; ++y) {
-			if (cell_at(shape_masks, x, y, z)) {
-				if (y < y0 + b->step_size) {
-					step_up = 1;
-					continue;
-				}
-				b->v.z = 0;
-				b->p.z = 0.5 * z - b->s.x - impulse;
-				if (b->cb_func) {
-					v3ll p(x >> 1, y >> 1, z >> 1);
-					b->cb_func(b, p, FACE_FT);
-				}
-				return;
-			}
+	bb = b->bb;
+	bb.z0 = bb.z1;
+	bb.z1 += b->v.z * dt;
+
+	bbc.x0 = floor(bb.x0);
+	bbc.y0 = floor(bb.y0);
+	bbc.z0 = floor(bb.z0);
+	bbc.x1 = ceil(bb.x1);
+	bbc.y1 = ceil(bb.y1);
+	bbc.z1 = ceil(bb.z1);
+
+	collide = false;
+	best = bb.z1;
+
+	for (auto p : bbc) {
+		Geom *g = geom[world->get_shape(p)];
+		if (g != nullptr && g->test_zpos(p, bb, &best)) {
+			collide = true;
+			best_p = p;
 		}
 	}
-	if (step_up)
-		b->p.y = 0.5 * (y0 + b->step_size) + b->s.y + impulse;
+	if (collide) {
+		b->v.z = 0;
+		b->p.z = best - b->s.x - impulse;
+		if (b->cb_func)
+			b->cb_func(b, best_p, FACE_BK);
+		return;
+	}
 	b->p.z += b->v.z * dt;
 }
 
 void Space::move_zneg(Body *b, float dt)
 {
-	int64_t x, y, z, x0, x1, y0, y1;
-	int step_up = 0;
+	box3f bb;
+	box3ll bbc;
+	bool collide;
+	float best;
+	v3ll best_p;
 
-	z = floor((b->bb.z0 + b->v.z * dt) * 2);
-	x0 = floor(b->bb.x0 * 2);
-	y0 = floor(b->bb.y0 * 2);
-	x1 = floor(b->bb.x1 * 2);
-	y1 = floor(b->bb.y1 * 2);
-	for (x = x0; x <= x1; ++x) {
-		for (y = y0; y <= y1; ++y) {
-			if (cell_at(shape_masks, x, y, z)) {
-				if (y < y0 + b->step_size) {
-					step_up = 1;
-					continue;
-				}
-				b->v.z = 0;
-				b->p.z = 0.5 * (z + 1) + b->s.x + impulse;
-				if (b->cb_func) {
-					v3ll p(x >> 1, y >> 1, z >> 1);
-					b->cb_func(b, p, FACE_BK);
-				}
-				return;
-			}
+	bb = b->bb;
+	bb.z1 = bb.z0;
+	bb.z0 += b->v.z * dt;
+
+	bbc.x0 = floor(bb.x0);
+	bbc.y0 = floor(bb.y0);
+	bbc.z0 = floor(bb.z0);
+	bbc.x1 = ceil(bb.x1);
+	bbc.y1 = ceil(bb.y1);
+	bbc.z1 = ceil(bb.z1);
+
+	collide = false;
+	best = bb.z0;
+
+	for (auto p : bbc) {
+		Geom *g = geom[world->get_shape(p)];
+		if (g != nullptr && g->test_zneg(p, bb, &best)) {
+			collide = true;
+			best_p = p;
 		}
 	}
-	if (step_up)
-		b->p.y = 0.5 * (y0 + b->step_size) + b->s.y + impulse;
+	if (collide) {
+		b->v.z = 0;
+		b->p.z = best + b->s.x + impulse;
+		if (b->cb_func)
+			b->cb_func(b, best_p, FACE_FT);
+		return;
+	}
 	b->p.z += b->v.z * dt;
 }
 
+/*
+	if (step_up)
+		b->p.y = 0.5 * (y0 + b->step_size) + b->s.y + impulse;
+	b->p.z += b->v.z * dt;
+*/
+
 void Space::move_ypos(Body *b, float dt)
 {
-	int64_t x, y, z, x0, x1, z0, z1;
+	box3f bb;
+	box3ll bbc;
+	bool collide;
+	float best;
+	v3ll best_p;
 
-	y = floor((b->bb.y1 + b->v.y * dt) * 2);
-	x0 = floor(b->bb.x0 * 2);
-	z0 = floor(b->bb.z0 * 2);
-	x1 = floor(b->bb.x1 * 2);
-	z1 = floor(b->bb.z1 * 2);
-	for (x = x0; x <= x1; ++x) {
-		for (z = z0; z <= z1; ++z) {
-			if (cell_at(shape_masks, x, y, z)) {
-				b->v.y = 0;
-				b->p.y = 0.5 * y - b->s.y - impulse;
-				if (b->cb_func) {
-					v3ll p(x >> 1, y >> 1, z >> 1);
-					b->cb_func(b, p, FACE_DN);
-				}
-				return;
-			}
+	bb = b->bb;
+	bb.y0 = bb.y1;
+	bb.y1 += b->v.y * dt;
+
+	bbc.x0 = floor(bb.x0);
+	bbc.y0 = floor(bb.y0);
+	bbc.z0 = floor(bb.z0);
+	bbc.x1 = ceil(bb.x1);
+	bbc.y1 = ceil(bb.y1);
+	bbc.z1 = ceil(bb.z1);
+
+	collide = false;
+	best = bb.y1;
+
+	for (auto p : bbc) {
+		Geom *g = geom[world->get_shape(p)];
+		if (g != nullptr && g->test_ypos(p, bb, &best)) {
+			collide = true;
+			best_p = p;
 		}
+	}
+	if (collide) {
+		b->v.y = 0;
+		b->p.y = best - b->s.y - impulse;
+		if (b->cb_func)
+			b->cb_func(b, best_p, FACE_DN);
+		return;
 	}
 	b->p.y += b->v.y * dt;
 }
 
 void Space::move_yneg(Body *b, float dt)
 {
-	int64_t x, y, z, x0, x1, z0, z1;
+	box3f bb;
+	box3ll bbc;
+	bool collide;
+	float best;
+	v3ll best_p;
 
-	y = floor((b->bb.y0 + b->v.y * dt) * 2);
-	x0 = floor(b->bb.x0 * 2);
-	z0 = floor(b->bb.z0 * 2);
-	x1 = floor(b->bb.x1 * 2);
-	z1 = floor(b->bb.z1 * 2);
-	for (x = x0; x <= x1; ++x) {
-		for (z = z0; z <= z1; ++z) {
-			if (cell_at(shape_masks, x, y, z)) {
-				b->v.y = 0;
-				b->p.y = 0.5 * (y + 1) + b->s.y + impulse;
-				if (b->cb_func) {
-					v3ll p(x >> 1, y >> 1, z >> 1);
-					b->cb_func(b, p, FACE_UP);
-				}
-				return;
-			}
+	bb = b->bb;
+	bb.y1 = bb.y0;
+	bb.y0 += b->v.y * dt;
+
+	bbc.x0 = floor(bb.x0);
+	bbc.y0 = floor(bb.y0);
+	bbc.z0 = floor(bb.z0);
+	bbc.x1 = ceil(bb.x1);
+	bbc.y1 = ceil(bb.y1);
+	bbc.z1 = ceil(bb.z1);
+
+	collide = false;
+	best = bb.y0;
+
+	for (auto p : bbc) {
+		Geom *g = geom[world->get_shape(p)];
+		if (g != nullptr && g->test_yneg(p, bb, &best)) {
+			collide = true;
+			best_p = p;
 		}
+	}
+	if (collide) {
+		b->v.y = 0;
+		b->p.y = best + b->s.y + impulse;
+		if (b->cb_func)
+			b->cb_func(b, best_p, FACE_UP);
+		return;
 	}
 	b->p.y += b->v.y * dt;
 }
