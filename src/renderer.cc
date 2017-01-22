887fefc32db9826a311cc9ce64c44de3bfe9c874
Luis Sanz <luis.sanz@gmail.com>
2017-01-22 22:08:38 +0100

    clean up renderer code; fix commandline color
    As far as the bug is concerned, the glColor and glBlendFunc functions
    were not called before drawing the commandline, so it was rendered with
    whatever color and function it was there.
    
    The rest is clean up and beautifying.

--- src/renderer.cc
+++ src/renderer.cc
@@ -220,29 +220,35 @@ void Renderer::render_item(int obj, int mat, GLfloat alpha)
 		obj_vertex_buffer->draw_slice(GL_TRIANGLES, 0, 282 * mat + 252, 36);
 		obj_vertex_buffer->disable();
 	}
+#if 0
+	if (s.obj == OBJ_TOKEN) {
+		v3f r = ctx->renderer->get_cam()->get_r();
+		glTranslatef(.25 / side, .25 / side, .25 / side);
+		glRotatef(180.0 * r.y / M_PI, 0, -1, 0);
+		glRotatef(180.0 * r.x / M_PI, 1, 0, 0);
+		glTranslatef(-.25 / side, -.25 / side, -.25 / side);
+	}
+#endif
 	shader->disable();
 }
 
 void Renderer::render_inventory(const std::vector<Item> &inv, const v3ll &p)
 {
-	int i, x, z;
-	int side = sqrt(inv.size());
-	Item s;
-
+	GLfloat side = sqrt(inv.size());
 	GLfloat d = dist(ctx->player->get_body()->get_p(), v3f(p));
 	GLubyte alpha = d > 4 ? 0 : d < 2 ? 255 : 255 * (2 - d / 2);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	i = 0;
-	for (x = 0; x < side; ++x) {
-		for (z = 0; z < side; ++z) {
-			glColor4ub(0, 0, 0, alpha / 2);
-			glMatrixMode(GL_MODELVIEW);
-			glPushMatrix();
-			glTranslatef(p.x + (GLfloat)x / side, p.y + 1.0001, p.z + (GLfloat)z / side);
+	int i = 0;
+	for (auto q : box2s(0, 0, side - 1, side - 1)) {
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+		glTranslatef(p.x + q.x / side, p.y + 1.0001, p.z + q.y / side);
+		{
 			glPushMatrix();
 			glTranslatef(.125 / side, 0, .125 / side);
 			glScalef(.75 / side, .75 / side, .75 / side);
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glColor4ub(0, 0, 0, alpha / 2);
 			glBegin(GL_TRIANGLES);
 			glVertex3f(0, 0, 0);
 			glVertex3f(0, 0, 1);
@@ -251,65 +257,55 @@ void Renderer::render_inventory(const std::vector<Item> &inv, const v3ll &p)
 			glVertex3f(0, 0, 1);
 			glVertex3f(1, 0, 1);
 			glEnd();
+			glDisable(GL_BLEND);
 			glPopMatrix();
-			Item s = inv[i];
-			if (s.num > 0) {
-				glColor4ub(0, 0, 0, alpha);
-				glPushMatrix();
-				glTranslatef(.5 / side - .0625, 0, .5 / side - .0625);
-#if 0
-				if (s.obj == OBJ_TOKEN) {
-					v3f r = ctx->renderer->get_cam()->get_r();
-					glTranslatef(.25 / side, .25 / side, .25 / side);
-					glRotatef(180.0 * r.y / M_PI, 0, -1, 0);
-					glRotatef(180.0 * r.x / M_PI, 1, 0, 0);
-					glTranslatef(-.25 / side, -.25 / side, -.25 / side);
-				}
-#endif
-				glScalef(.125, .125, .125);
-				render_item(s.obj, s.mat, alpha);
-				glPopMatrix();
-			}
-			if (s.num > 1) {
-				glColor4ub(255, 255, 255, alpha);
-				glMatrixMode(GL_MODELVIEW);
-				glPushMatrix();
-				glTranslatef(.5 / side, .5 / side, .5 / side);
-				glRotatef(180.0 * cam->get_r().y / M_PI, 0, -1, 0);
-				glRotatef(180.0 * cam->get_r().x / M_PI, 1, 0, 0);
-				glScalef(.05, .05, .05);
-				char buf[3];
-				snprintf(buf, sizeof(buf), "%02d", s.num);
-				render_string(buf);
-				glPopMatrix();
-			}
-			glPopMatrix();
-			++i;
 		}
+		Item s = inv[i];
+		if (s.num > 0) {
+			glPushMatrix();
+			glTranslatef(.5 / side - .0625, 0, .5 / side - .0625);
+			glScalef(.125, .125, .125);
+			render_item(s.obj, s.mat, alpha);
+			glPopMatrix();
+		}
+		if (s.num > 1) {
+			glMatrixMode(GL_MODELVIEW);
+			glPushMatrix();
+			glTranslatef(.5 / side, .5 / side, .5 / side);
+			glRotatef(180.0 * cam->get_r().y / M_PI, 0, -1, 0);
+			glRotatef(180.0 * cam->get_r().x / M_PI, 1, 0, 0);
+			glScalef(.05, .05, .05);
+			char buf[3];
+			snprintf(buf, sizeof(buf), "%02d", s.num);
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glColor4ub(255, 255, 255, alpha);
+			render_string(buf);
+			glDisable(GL_BLEND);
+			glPopMatrix();
+		}
+		glPopMatrix();
+		++i;
 	}
-	glDisable(GL_BLEND);
 }
 
 void Renderer::render_board(const std::vector<Item> &inv, const v3ll &p)
 {
-	int i, x, z;
-	int side = sqrt(inv.size());
-	Item s;
-
+	GLfloat side = sqrt(inv.size());
 	GLfloat d = dist(ctx->player->get_body()->get_p(), v3f(p));
 	GLubyte alpha = d > 4 ? 0 : d < 2 ? 255 : 255 * (2 - d / 2);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	i = 0;
-	for (x = 0; x < side; ++x) {
-		for (z = 0; z < side; ++z) {
-			glColor4ub(0, 0, 0, alpha / 2);
-			glMatrixMode(GL_MODELVIEW);
-			glPushMatrix();
-			glTranslatef(p.x + (GLfloat)x / side, p.y + 1.0001, p.z + (GLfloat)z / side);
+	int i = 0;
+	for (auto q : box2s(0, 0, side - 1, side - 1)) {
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+		glTranslatef(p.x + q.x / side, p.y + 1.0001, p.z + q.y / side);
+		{
 			glPushMatrix();
 			glTranslatef(.125 / side, 0, .125 / side);
 			glScalef(.75 / side, .75 / side, .75 / side);
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glColor4ub(0, 0, 0, alpha / 2);
 			glBegin(GL_TRIANGLES);
 			glVertex3f(0, 0, 0);
 			glVertex3f(0, 0, 1);
@@ -318,56 +314,57 @@ void Renderer::render_board(const std::vector<Item> &inv, const v3ll &p)
 			glVertex3f(0, 0, 1);
 			glVertex3f(1, 0, 1);
 			glEnd();
+			glDisable(GL_BLEND);
 			glPopMatrix();
-			Item s = inv[i];
-			if (s.num > 0) {
-				glColor4ub(0, 0, 0, alpha);
-				glPushMatrix();
-				glTranslatef(.5 / side - .06, 0, .5 / side - .06);
-#if 0
-				if (s.obj == OBJ_TOKEN) {
-					v3f r = ctx->renderer->get_cam()->get_r();
-					glTranslatef(.25 / side, .25 / side, .25 / side);
-					glRotatef(180.0 * r.y / M_PI, 0, -1, 0);
-					glRotatef(180.0 * r.x / M_PI, 1, 0, 0);
-					glTranslatef(-.25 / side, -.25 / side, -.25 / side);
-				}
-#endif
-				glScalef(.12, .12, .12);
-				render_item(s.obj, s.mat, alpha);
-				glPopMatrix();
-			}
-			if ((s.num & 2) != 0) {
-				glColor4ub(255, 255, 255, alpha);
-				glMatrixMode(GL_MODELVIEW);
-				glPushMatrix();
-				glTranslatef(.5 / side, .5 / side, .5 / side);
-				glRotatef(180.0 * cam->get_r().y / M_PI, 0, -1, 0);
-				glRotatef(180.0 * cam->get_r().x / M_PI, 1, 0, 0);
-				glScalef(.05, .05, .05);
-				render_string("ON");
-				glPopMatrix();
-			}
-			glPopMatrix();
-			++i;
 		}
+		Item s = inv[i];
+		if (s.num > 0) {
+			glColor4ub(0, 0, 0, alpha);
+			glPushMatrix();
+			glTranslatef(.5 / side - .06, 0, .5 / side - .06);
+			glScalef(.12, .12, .12);
+			render_item(s.obj, s.mat, alpha);
+			glPopMatrix();
+		}
+		if ((s.num & 2) != 0) {
+			glMatrixMode(GL_MODELVIEW);
+			glPushMatrix();
+			glTranslatef(.5 / side, .5 / side, .5 / side);
+			glRotatef(180.0 * cam->get_r().y / M_PI, 0, -1, 0);
+			glRotatef(180.0 * cam->get_r().x / M_PI, 1, 0, 0);
+			glScalef(.05, .05, .05);
+			glColor4ub(255, 255, 255, alpha);
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			render_string("ON");
+			glDisable(GL_BLEND);
+			glPopMatrix();
+		}
+		glPopMatrix();
+		++i;
 	}
-	glDisable(GL_BLEND);
 }
 
 void Renderer::render_commandline()
 {
 	if (ctx->mode == MODE_COMMAND) {
-		glEnable(GL_BLEND);
-		glColor3f(1, 1, 1);
 		v3f p = cam->get_p();
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
 		glTranslatef(p.x, p.y, p.z);
 		glRotatef(180.0 * cam->get_r().y / M_PI, 0, -1, 0);
 		glRotatef(180.0 * cam->get_r().x / M_PI, 1, 0, 0);
 		glTranslatef(-15, 0, -30);
-		if (ctx->cli->get_visible())
+		if (ctx->cli->get_visible()) {
+			glColor3f(1, 1, 1);
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 			render_string(ctx->cli->get_visible());
+			glDisable(GL_BLEND);
+		}
 		glTranslatef(.5 * ctx->cli->get_cur_char(), 0, 0);
+		glColor3f(1, 1, 1);
+		glEnable(GL_BLEND);
 		glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_ZERO);
 		glBegin(GL_TRIANGLES);
 		glVertex3f(.5, 1, 0);
@@ -378,8 +375,8 @@ void Renderer::render_commandline()
 		glVertex3f(0, 0, 0);
 		glEnd();
 		glDisable(GL_BLEND);
+		glPopMatrix();
 	}
-	glEnable(GL_DEPTH_TEST);
 }
 
 void Renderer::render_shards()
