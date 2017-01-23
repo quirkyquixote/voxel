5cb0e4159cc1952b971ace159dfaaa6c7c1d821b
Luis Sanz <luis.sanz@gmail.com>
2017-01-23 13:37:19 +0100

    Context: make use of box iterators and operators

--- src/context.cc
+++ src/context.cc
@@ -86,9 +86,6 @@ Context::~Context()
 
 bool Context::load_all()
 {
-	int x, z;
-	Chunk *c;
-
 	mkpath(dir, 0700);
 	load_world();
 	if (!load_player()) {
@@ -97,17 +94,12 @@ bool Context::load_all()
 		p.y += CHUNK_W * CHUNKS_PER_WORLD / 2;
 		player->get_body()->set_p(v3f(p.x, WORLD_H, p.y));
 	}
-	for (x = 0; x < CHUNKS_PER_WORLD; ++x) {
-		for (z = 0; z < CHUNKS_PER_WORLD; ++z) {
-			c = world->get_chunk(v2ll(x, z));
-			v2ll p = world->get_p();
-			p.x += x * CHUNK_W;
-			p.y += z * CHUNK_D;
-			c->set_p(p);
-			if (!load_chunk(c)) {
-				terraform(0, c);
-				c->set_flags(CHUNK_UNLIT);
-			}
+	for (auto p : box2ll(0, 0, CHUNKS_PER_WORLD - 1, CHUNKS_PER_WORLD -1)) {
+		Chunk *c = world->get_chunk(p);
+		c->set_p(world->get_p() + p * v2ll(CHUNK_W, CHUNK_D));
+		if (!load_chunk(c)) {
+			terraform(0, c);
+			c->set_flags(CHUNK_UNLIT);
 		}
 	}
 	return true;
@@ -115,12 +107,10 @@ bool Context::load_all()
 
 void Context::save_all()
 {
-	v2ll p;
 	save_world();
 	save_player();
-	for (p.x = 0; p.x < CHUNKS_PER_WORLD; ++p.x)
-		for (p.y = 0; p.y < CHUNKS_PER_WORLD; ++p.y)
-			save_chunk(world->get_chunk(p));
+	for (auto p : box2ll(0, 0, CHUNKS_PER_WORLD - 1, CHUNKS_PER_WORLD - 1))
+		save_chunk(world->get_chunk(p));
 }
 
 bool Context::load_world()
@@ -299,31 +289,18 @@ void Context::drop_block(const v3ll &p)
 
 void Context::update_chunks()
 {
-	int x, z;
-	Chunk *c;
 	std::map<int, Chunk *> out_of_date;
-	box2ll bb;
-	v3ll m;
-
 	v2ll p(player->get_body()->get_p().x, player->get_body()->get_p().z);
-	bb.x0 = floor(p.x) - CHUNK_W * CHUNKS_PER_WORLD / 2;
-	bb.y0 = floor(p.y) - CHUNK_D * CHUNKS_PER_WORLD / 2;
-	bb.x1 = bb.x0 + CHUNK_W * CHUNKS_PER_WORLD;
-	bb.y1 = bb.y0 + CHUNK_D * CHUNKS_PER_WORLD;
-	m.x = CHUNK_W * CHUNKS_PER_WORLD * floor(bb.x1 / (CHUNK_W * CHUNKS_PER_WORLD));
-	m.y = CHUNK_D * CHUNKS_PER_WORLD * floor(bb.y1 / (CHUNK_D * CHUNKS_PER_WORLD));
 
-	for (x = 0; x < CHUNKS_PER_WORLD; ++x) {
-		for (z = 0; z < CHUNKS_PER_WORLD; ++z) {
-			c = world->get_chunk(v2ll(x, z));
-			if (c->get_flags() != 0) {
-				int d = dist(c->get_p(), p);
-				out_of_date.emplace(d, c);
-			}
+	for (auto q : box2ll(0, 0, CHUNKS_PER_WORLD - 1, CHUNKS_PER_WORLD - 1)) {
+		Chunk *c = world->get_chunk(q);
+		if (c->get_flags() != 0) {
+			int d = dist(c->get_p(), p);
+			out_of_date.emplace(d, c);
 		}
 	}
 	if (out_of_date.empty()) {
-//		log_info("no chunks to update");
+		//		log_info("no chunks to update");
 		return;
 	}
 
@@ -331,15 +308,15 @@ void Context::update_chunks()
 	for (auto &iter : out_of_date) {
 		if (i >= chunks_per_tick)
 			break;
-		c = iter.second;
-//		log_info("Update chunk %d (%lld,%lld); priority:%d", c->get_id(), c->get_p().x, c->get_p().y, iter.first);
+		Chunk *c = iter.second;
+		//		log_info("Update chunk %d (%lld,%lld); priority:%d", c->get_id(), c->get_p().x, c->get_p().y, iter.first);
 		if ((c->get_flags() & CHUNK_UNLOADED) != 0) {
-//			log_info("load from file");
+			//			log_info("load from file");
 			/* load this chunk */
 			c->unset_flags(CHUNK_UNLOADED);
 		}
 		if ((c->get_flags() & CHUNK_UNLIT) != 0) {
-//			log_info("lit up");
+			//			log_info("lit up");
 			box3ll bb;
 			bb.x0 = c->get_p().x;
 			bb.y0 = 0;
@@ -352,7 +329,7 @@ void Context::update_chunks()
 			c->set_flags(CHUNK_UNRENDERED);
 		}
 		if ((c->get_flags() & CHUNK_UNRENDERED) != 0) {
-//			log_info("update vbos");
+			//			log_info("update vbos");
 			for (int k = 0; k < SHARDS_PER_CHUNK; ++k)
 				renderer->update_shard(c->get_shard(k)->get_id(), c->get_p().x,
 						k * SHARD_H, c->get_p().y);
@@ -360,7 +337,7 @@ void Context::update_chunks()
 		}
 		++i;
 	}
-//		log_info("update %d of %zd chunks (max: %d)", i, out_of_date.size(), chunks_per_tick);
+	//		log_info("update %d of %zd chunks (max: %d)", i, out_of_date.size(), chunks_per_tick);
 }
 
 void Context::update()
@@ -368,7 +345,7 @@ void Context::update()
 	if (!ml->get_window()->has_mouse_focus())
 		return;
 	space->step();
-/*	if ((tick & 1) == 0)
+	/*	if ((tick & 1) == 0)
 		flowsim_step(flowsim);*/
 	player->update();
 	renderer->update_camera();
@@ -426,7 +403,7 @@ void Context::event(const SDL_Event &e)
 			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		} else if (e.key.keysym.sym == SDLK_p) {
 			fprintf(stdout, "=== PROFILE START ===\n");
-		//	profile_manager_reset(prof_mgr);
+			//	profile_manager_reset(prof_mgr);
 		}
 	} else if (e.type == SDL_KEYUP) {
 		if (e.key.repeat) {
