30ca12c4a1892138916c802d4e9500ae6a648392
Luis Sanz <luis.sanz@gmail.com>
2017-01-25 23:28:51 +0100

    fix issue with chunk generation for indices < 0
    A casting error produced a bounding box of the wrong size.

--- src/terraform.cc
+++ src/terraform.cc
@@ -2,6 +2,8 @@
 
 #include "terraform.h"
 
+#include <limits.h>
+
 #include <libnoise/noise.h>
 #include "noiseutils.h"
 
@@ -10,16 +12,22 @@
 void terraform(int64_t seed, Chunk *c)
 {
 	v2ll p0 = c->get_p();
-	box2f bb(p0.x, p0.y, p0.x + Chunk::W, p0.y + Chunk::D);
-	bb /= 1000.f;
+	box2f bb(0, 0, Chunk::W, Chunk::D);
+	bb += v2f(p0);
+	bb /= 500.f;
 
-	noise::module::RidgedMulti mountainTerrain;
+	noise::module::RidgedMulti baseMountainTerrain;
+	noise::module::ScaleBias mountainTerrain;
+	mountainTerrain.SetSourceModule (0, baseMountainTerrain);
+	mountainTerrain.SetScale (0.5);
+	mountainTerrain.SetBias (0.5);
+
 	noise::module::Billow baseFlatTerrain;
 	baseFlatTerrain.SetFrequency (2.0);
 	noise::module::ScaleBias flatTerrain;
 	flatTerrain.SetSourceModule (0, baseFlatTerrain);
 	flatTerrain.SetScale (0.125);
-	flatTerrain.SetBias (-0.75);
+	flatTerrain.SetBias (0.125);
 
 	noise::module::Perlin terrainType;
 	terrainType.SetFrequency (0.5);
@@ -44,8 +52,20 @@ void terraform(int64_t seed, Chunk *c)
 		v3ll q(p.x, 0, p.y);
 		float f = heightMap.GetValue(p.x, p.y);
 		int h = 128 + f * 64;
+		int mat;
+		if (f < .1)
+			mat = MAT_SANDSTONE_SAND;
+		if (f < .5)
+			mat = MAT_GRASS;
+		else if (f < .6)
+			mat = MAT_LIMESTONE_SAND;
+		else if (f < .75)
+			mat = MAT_LIMESTONE;
+		else
+			mat = MAT_MARBLE;
+	//	log_info("%lld,%lld = %d (%s)", p.x + p0.x, p.y + p0.y, h, mat_names[mat]);
 		while(q.y < h) {
-			c->set_mat(q, MAT_LIMESTONE);
+			c->set_mat(q, mat);
 			c->set_shape(q, SHAPE_BLOCK_DN);
 			++q.y;
 		}
